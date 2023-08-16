c21b9a27abde9a331b7b476fdd24127e321d6f46
Thierry Murhondezi Bisimwa <thierrymurhondezi@gmail.com>
2023-08-16 04:50:56 +0200

    add : Function to render tasks on the page

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ const tasks = [
     { id: 1, description: "Task 1", completed: false },
     { id: 2, description: "Task 2", completed: true },
   ];
+
+  // Function to render tasks on the page
+function renderTasks() {
+    const taskList = document.getElementById("task-list");
